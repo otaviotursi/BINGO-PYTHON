f4f00032724b2abcf2ca720367be96bd2e59fc97
otaviotursi <62893377+otaviotursi@users.noreply.github.com>
2021-04-29 01:33:57 -0300

    atualizacao

---
+++ gerar cartelas/ajustar-cartelas.py
@@ -0,0 +1,91 @@
+import cv2
+from os import walk
+import numpy as np
+import time
+import shutil, os
+
+#programa certoooooooooooooooooooooooooooooooo
+
+#primeiro faz essa parte, que diminui o tamanho da cartela, depoois vamos imprimir em um papel A4
+
+def ajusteTamanho():
+    # verifica todas as imgs da pasta
+    f = []
+    for (dirpath, dirnames, filenames) in walk('cartelas-numeradas/'):
+        f.extend(filenames)
+        break
+
+    for picture in (f):
+
+        # caminho da foto
+        pic = 'cartelas-numeradas/' + picture
+
+        # lê e reduz 2.2 vezes a imagem
+        img = cv2.imread(pic)
+        resized = cv2.resize(img, (int(img.shape[1]/2.2), int(img.shape[0]/2.2)), 3)
+
+        # escreve a cartela reduzida dentro da pasta "cartelas-ajustadas-tamanho"
+        cv2.imwrite(f'cartelas-ajustadas-tamanho/resized_{picture}', resized)
+        time.sleep(0.5)
+    
+    for pic in f:
+        if 'CARTELA_ANOTACOES' not in pic:
+            shutil.move(f'cartelas-numeradas/{pic}', f'cartelas-usadas/{pic}')
+
+def imprimirA4():
+    qntFolhasImpressao = 5
+    
+    for x in range(qntFolhasImpressao):
+
+        # verifica todas as imgs da pasta
+        f = []
+        for (dirpath, dirnames, filenames) in walk('cartelas-ajustadas-tamanho/'):
+            f.extend(filenames)
+            break
+        
+        # le todas as cartelas definidas de 5 em 5, supondo que são 5 rodadas
+        tamReduzidoList = []
+        for n, a in enumerate(f):
+            # vai adicionar a lista de imagens, aquelas que tem "resized"  no nome, também que tenha "R" e que não seja repedido e não adicionar imagens de anotações
+            if a.startswith('resized') and f'R{n}' not in tamReduzidoList and 'resized_CARTELA_ANOTACOES' not in tamReduzidoList:
+                # salvando...
+                tamReduzidoList.append(a)
+                # se a quantidade de rodadas/folhas chegar a 5, então para de salvar imgs
+                if len(tamReduzidoList) >= 5:
+                    break
+        
+        # salvando imagem de anotações
+        anot = []
+        for n, a in enumerate(f):
+            if a == 'resized_CARTELA_ANOTACOES.jpg':
+                anot.append(a)
+
+        # lendo as imagens
+        img1 = cv2.imread(f'cartelas-ajustadas-tamanho/{tamReduzidoList[0]}')
+        img2 = cv2.imread(f'cartelas-ajustadas-tamanho/{tamReduzidoList[1]}')
+        img3 = cv2.imread(f'cartelas-ajustadas-tamanho/{tamReduzidoList[2]}')
+        img4 = cv2.imread(f'cartelas-ajustadas-tamanho/{tamReduzidoList[3]}')
+        img5 = cv2.imread(f'cartelas-ajustadas-tamanho/{tamReduzidoList[4]}')
+        img6 = cv2.imread(f'cartelas-ajustadas-tamanho/{anot[0]}')
+
+        # ajusta as imagens na folha 
+        img_tile = concat_vh([[img1, img2, img3], 
+                        [img4, img5, img6], 
+                        ]) 
+
+        # imprime as imagens
+        cv2.imwrite(f'cartelas-impressao/Jogo_CARTELA{x}.jpg', img_tile)
+
+        # move as magens já utilizadas para outra pasta
+        for pic in tamReduzidoList:
+            shutil.move(f'cartelas-ajustadas-tamanho/{pic}', f'cartelas-usadas/{pic}')
+
+def concat_vh(list_2d): 
+    # return final image 
+    return cv2.vconcat([cv2.hconcat(list_h) for list_h in list_2d]) 
+
+
+ajusteTamanho()
+imprimirA4()
+
+
---
+++ gerar cartelas/gerador-cartelas.py
@@ -0,0 +1,147 @@
+import random
+import time
+import re
+import cv2
+import numpy as np
+import sys
+import os
+
+B = []
+I = []
+N = []
+G = []
+O = []
+
+
+def BdeBingo():
+    # vai sortear entre os dois valores
+    Bingo = random.randint(1, 15)
+    if Bingo <= 9:
+        Bingo = "0" + str(Bingo)
+
+    # se o valor ja estiver dentro da lista, nao faz nada
+    if Bingo not in B:
+        B.append(Bingo)
+    
+    # enquanto a lista for diferente de 5, a funcao vai continuar sendo chamada
+    if len(B) != 5:
+        BdeBingo()
+
+
+def IdeBingo():
+    # vai sortear entre os dois valores
+    Bingo = random.randint(16, 30)
+
+    # se o valor ja estiver dentro da lista, nao faz nada
+    if Bingo not in I:
+        I.append(Bingo)
+
+    # enquanto a lista for diferente de 5, a funcao vai continuar sendo chamada
+    if len(I) != 5:
+        IdeBingo()
+
+
+def NdeBingo():
+    # vai sortear entre os dois valores
+    Bingo = random.randint(31, 45)
+    
+    # se o valor ja estiver dentro da lista, nao faz nada
+    if Bingo not in N:
+        N.append(Bingo)
+
+    # enquanto a lista for diferente de 5, a funcao vai continuar sendo chamada
+    if len(N) != 5:
+        NdeBingo()
+
+
+def GdeBingo():
+    # vai sortear entre os dois valores
+    Bingo = random.randint(46, 60)
+
+    # se o valor ja estiver dentro da lista, nao faz nada
+    if Bingo not in G:
+        G.append(Bingo)
+
+    # enquanto a lista for diferente de 5, a funcao vai continuar sendo chamada
+    if len(G) != 5:
+        GdeBingo()
+
+
+def OdeBingo():
+    # vai sortear entre os dois valores
+    Bingo = random.randint(61, 75)
+
+    # se o valor ja estiver dentro da lista, nao faz nada
+    if Bingo not in O:
+        O.append(Bingo)
+        
+    # enquanto a lista for diferente de 5, a funcao vai continuar sendo chamada
+    if len(O) != 5:
+        OdeBingo()
+
+
+def sortearNumeros():
+    # limpa a lista
+    B.clear()
+    I.clear()
+    N.clear()
+    G.clear()
+    O.clear()
+
+    # sorteia os numeros de cada tabela
+    BdeBingo()
+    IdeBingo()
+    NdeBingo()
+    GdeBingo()
+    OdeBingo()
+
+
+
+while True:
+    # input de quantidade de cartelas e quantidade de rodadas
+    qntCartelas = input("Quantas cartelas você deseja? ")
+    rodadasCartelas = input("Para qual rodada essas cartelas irão valer? ")
+
+
+    quantidadeCartelas = 1
+    while quantidadeCartelas <= int(qntCartelas):
+
+        # chama a funcao de sortear os numeros
+        sortearNumeros()
+
+        # apresentacao dos numeros da cartela e print 
+        print('------------------------')
+        print(f"ID: {quantidadeCartelas} R:{rodadasCartelas}")
+        print(f'B  | I  | N  | G  | O  ')
+        
+
+        # jogando os valores sorteados dentro do papel (cartela a ser impressa)
+        x = 10
+        y = 100
+        img = cv2.imread("bingo_cartela.jpg")
+        for i in range(len(B)):
+            print(f'{B[i]} | {I[i]} | {N[i]} | {G[i]} | {O[i]}')
+            
+            # informações inicias dentro da cartela
+            cv2.putText(img, str('ID:' + str(quantidadeCartelas)), (60, 140), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255), thickness=2)
+            cv2.putText(img, ("R:" + str(rodadasCartelas)), (460, 140), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), thickness=2)
+            
+            # dispoê o numero da letra B, (coluna x linha)
+            cv2.putText(img, str(B[i]), (60, (250 + (116 * i))), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), thickness=2)
+            # dispoê o numero da letra i, (coluna x linha)
+            cv2.putText(img, str(I[i]), (170, (250 + (116 * i))), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), thickness=2)
+
+            # dispoê o numero da letra N, (coluna x linha)
+            if not i == 2:
+                cv2.putText(img, str(N[i]), (290, (250 + (116 * i))), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), thickness=2)
+
+            # dispoê o numero da letra G, (coluna x linha)
+            cv2.putText(img, str(G[i]), (400, (250 + (116 * i))), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), thickness=2)
+            # dispoê o numero da letra O, (coluna x linha)
+            cv2.putText(img, str(O[i]), (515, (250 + (116 * i))), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), thickness=2)
+
+        cv2.imwrite('cartelas-numeradas/CARTELA_' + str(quantidadeCartelas) + '_R' + str(rodadasCartelas) + '.jpg', img)
+        cv2.destroyWindow("bingo_cartela.jpg")
+        quantidadeCartelas += 1
+
+        print('------------------------')
